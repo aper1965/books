9db2dafe7fab6be58584d332a32dd26e5453b569
Anders Granlöf <anders.granlof@gmail.com>
2025-10-28 16:59:24 +0100

    Cleaning

--- app/src/main/java/com/example/mybooks/MainActivity.kt
+++ app/src/main/java/com/example/mybooks/MainActivity.kt
@@ -7,17 +7,12 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.runtime.Composable
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.example.mybooks.model.BooksViewModel
-import com.example.mybooks.model.WriterSimple
+import com.example.mybooks.ui.screens.BookList
 import com.example.mybooks.ui.screens.WriterList
 import com.example.mybooks.ui.theme.MyBooksTheme
-import kotlinx.serialization.Serializable
-import androidx.navigation.compose.composable
-import com.example.mybooks.ui.screens.BookList
-
-@Serializable
-data class Writers(val writers: ArrayList<WriterSimple>)
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -25,7 +20,7 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             MyBooksTheme {
-                val vm = ViewModelProvider(this).get(BooksViewModel::class.java)
+                val vm = ViewModelProvider(this)[BooksViewModel::class.java]
                 StartBook(vm)
             }
         }
